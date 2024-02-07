a27873b089b9ef186aaec68428640a02f553060f
Vicente Espejo <viespejo@gmail.com>
2024-02-07 10:35:13 +0100

    add luasnip custom config and modify cmp config

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -9,18 +9,19 @@ local M = {
 			"hrsh7th/cmp-emoji",
 			event = "InsertEnter",
 		},
-		-- {
-		-- 	"hrsh7th/cmp-buffer",
-		-- 	event = "InsertEnter",
-		-- },
-		-- {
-		-- 	"hrsh7th/cmp-path",
-		-- 	event = "InsertEnter",
-		-- },
-		-- {
-		-- 	"hrsh7th/cmp-cmdline",
-		-- 	event = "InsertEnter",
-		-- },
+		{ "onsails/lspkind-nvim" },
+		{
+			"hrsh7th/cmp-buffer",
+			event = "InsertEnter",
+		},
+		{
+			"hrsh7th/cmp-path",
+			event = "InsertEnter",
+		},
+		{
+			"hrsh7th/cmp-cmdline",
+			event = "InsertEnter",
+		},
 		{
 			"saadparwaiz1/cmp_luasnip",
 			event = "InsertEnter",
@@ -28,9 +29,6 @@ local M = {
 		{
 			"L3MON4D3/LuaSnip",
 			event = "InsertEnter",
-			dependencies = {
-				"rafamadriz/friendly-snippets",
-			},
 		},
 		{
 			"hrsh7th/cmp-nvim-lua",
@@ -47,11 +45,6 @@ function M.config()
 	-- 	color_square_width = 2,
 	-- })
 
-	vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-	vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
-	vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
-	vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
-
 	local cmp = require("cmp")
 
 	local luasnip = require("luasnip")
@@ -60,13 +53,15 @@ function M.config()
 	require("luasnip.loaders.from_lua").lazy_load({ paths = "./lua/snippets" })
 	require("luasnip").filetype_extend("typescriptreact", { "html" })
 
-	local check_backspace = function()
-		local col = vim.fn.col(".") - 1
-		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-	end
-
-	local icons = require("user.icons")
+	-- local icons = require("user.icons")
 	local types = require("cmp.types")
+	local lspkind = require("lspkind")
+
+	-- vim.api.nvim_set_hl(0, "CmpItemAbbr", { link = "Comment" })
+	-- vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { link = "Error" })
+	-- vim.api.nvim_set_hl(0, "CmpItemKind", { link = "Special" })
+	-- vim.api.nvim_set_hl(0, "CmpItemMenu", { link = "NonText" })
+	vim.api.nvim_set_hl(0, "MyPmenuSel", { link = "Todo" })
 
 	cmp.setup({
 		snippet = {
@@ -78,188 +73,119 @@ function M.config()
 			autocomplete = false, -- disable auto-completion
 		},
 		mapping = cmp.mapping.preset.insert({
-			-- ["<C-k>"] = cmp.mapping(
-			-- 	cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
-			-- 	{ "i", "c" }
-			-- ),
-			-- ["<C-j>"] = cmp.mapping(
-			-- 	cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
-			-- 	{ "i", "c" }
-			-- ),
 			["<C-p>"] = cmp.mapping(
-				cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
+				cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
 				{ "i", "c" }
 			),
 			["<C-n>"] = cmp.mapping(
-				cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
+				cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
 				{ "i", "c" }
 			),
-			["<C-h>"] = function()
-				if cmp.visible_docs() then
-					cmp.close_docs()
-				else
-					cmp.open_docs()
-				end
-			end,
-			-- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-			-- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			-- ["<C-h>"] = function()
+			-- 	if cmp.visible_docs() then
+			-- 		cmp.close_docs()
+			-- 	else
+			-- 		cmp.open_docs()
+			-- 	end
+			-- end,
 			["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 			["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-			["<C-e>"] = cmp.mapping({
-				i = cmp.mapping.abort(),
-				c = cmp.mapping.close(),
-			}),
-			-- Accept currently selected item. If none selected, `select` first item.
-			-- Set `select` to `false` to only confirm explicitly selected items.
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expandable() then
-					luasnip.expand()
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				elseif check_backspace() then
-					fallback()
-					-- require("neotab").tabout()
-				else
-					fallback()
-					-- require("neotab").tabout()
-				end
-			end, {
-				"i",
-				"s",
-			}),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, {
-				"i",
-				"s",
-			}),
+			-- ["<C-d>"] = function()
+			-- 	if cmp.visible_docs() then
+			-- 		cmp.close_docs()
+			-- 	else
+			-- 		cmp.open_docs()
+			-- 	end
+			-- end,
+			-- -- tab and shift-tab to navigate through snippets once expanded
+			-- ["<Tab>"] = cmp.mapping(function(fallback)
+			-- 	if luasnip.expand_or_jumpable() then
+			-- 		luasnip.expand_or_jump()
+			-- 	else
+			-- 		fallback()
+			-- 	end
+			-- end, {
+			-- 	"i",
+			-- 	"s",
+			-- }),
+			-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+			-- 	if luasnip.jumpable(-1) then
+			-- 		luasnip.jump(-1)
+			-- 	else
+			-- 		fallback()
+			-- 	end
+			-- end, {
+			-- 	"i",
+			-- 	"s",
+			-- }),
 		}),
 		formatting = {
-			fields = { "kind", "abbr", "menu" },
-			format = function(entry, vim_item)
-				vim_item.kind = icons.kind[vim_item.kind]
-				vim_item.menu = ({
+			-- fields = { "abbr", "kind", "menu" },
+			format = lspkind.cmp_format({
+				-- maxwidth = 50,
+				menu = {
+					buffer = "[buf]",
 					nvim_lsp = "[LSP]",
-					nvim_lua = "[NVIM_LUA]",
-					luasnip = "[Snippet]",
-					-- buffer = "[Buffer]",
-					-- path = "[Path]",
-					emoji = "[Emoji]",
-					["vim-dadbod-completion"] = "[DB]",
-				})[entry.source.name]
-
-				if vim.tbl_contains({ "nvim_lsp" }, entry.source.name) then
-					local duplicates = {
-						buffer = 1,
-						path = 1,
-						nvim_lsp = 0,
-						luasnip = 1,
-					}
-
-					local duplicates_default = 0
-
-					vim_item.dup = duplicates[entry.source.name] or duplicates_default
-				end
-
-				if entry.source.name == "copilot" then
-					vim_item.kind = icons.git.Octoface
-					vim_item.kind_hl_group = "CmpItemKindCopilot"
-				end
-
-				if entry.source.name == "emoji" then
-					vim_item.kind = icons.misc.Smiley
-					vim_item.kind_hl_group = "CmpItemKindEmoji"
-				end
-
-				return vim_item
-			end,
+					nvim_lua = "[nvim_lua]",
+					path = "[path]",
+					luasnip = "[snip]",
+					emoji = "[emoji]",
+					["vim-dadbod-completion"] = "[db]",
+				},
+			}),
 		},
-		sources = {
-			{ name = "copilot" },
-			{
-				name = "nvim_lsp",
-				entry_filter = function(entry, ctx)
-					local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
-					if kind == "Snippet" and ctx.prev_context.filetype == "java" then
-						return false
-					end
-
-					if ctx.prev_context.filetype == "markdown" then
-						return true
-					end
-
-					if kind == "Text" then
-						return false
-					end
-
-					return true
-				end,
-			},
+		-- view = {
+		-- 	docs = {
+		-- 		auto_open = false,
+		-- 	},
+		-- },
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp", keyword_length = 3 },
 			{ name = "luasnip" },
 			{ name = "nvim_lua" },
-			-- { name = "buffer" },
-			-- { name = "path" },
+			{ name = "buffer" },
+			{ name = "path" },
 			{ name = "emoji" },
-		},
-		confirm_opts = {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = false,
-		},
-		view = {
-			entries = {
-				name = "custom",
-				selection_order = "top_down",
-			},
-			docs = {
-				auto_open = false,
-			},
-		},
+		}),
 		window = {
 			completion = {
-				border = "rounded",
-				winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
-				col_offset = -3,
-				side_padding = 1,
-				scrollbar = false,
-				scrolloff = 8,
+				winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:MyPmenuSel,Search:None",
 			},
-			documentation = {
-				border = "rounded",
-				winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
-			},
-		},
-		experimental = {
-			ghost_text = false,
 		},
 	})
 
-	local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = { "sql", "mysql", "plsql" },
-		callback = function()
-			cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-		end,
-		group = autocomplete_group,
+	-- Set configuration for specific filetype.
+	cmp.setup.filetype("gitcommit", {
+		sources = cmp.config.sources({
+			{ name = "buffer" },
+		}),
 	})
 
-	-- vim.pcall(function()
-	-- 	cmp.event:on("menu_opened", function()
-	-- 		vim.b.copilot_suggestion_hidden = true
-	-- 	end)
-	-- 	cmp.event:on("menu_closed", function()
-	-- 		vim.b.copilot_suggestion_hidden = false
-	-- 	end)
-	-- end)
+	cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+		sources = cmp.config.sources({
+			{ name = "vim-dadbod-completion" },
+			{ name = "buffer" },
+		}),
+	})
+
+	-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+	cmp.setup.cmdline({ "/", "?" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+	})
+
+	-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
 end
 
 return M
---
+++ lua/user/luasnip.lua
@@ -0,0 +1,61 @@
+local M = {
+	"L3MON4D3/LuaSnip",
+	event = "InsertEnter",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
+}
+
+function M.config()
+	local ls = require("luasnip")
+	local types = require("luasnip.util.types")
+
+	ls.config.set_config({
+		-- This tells LuaSnip to remember to keep around the last snippet.
+		-- You can jump back into it even if you move outside of the selection
+		history = false,
+
+		-- This one is cool cause if you have dynamic snippets, it updates as you type!
+		updateevents = "TextChanged,TextChangedI",
+
+		-- Autosnippets:
+		enable_autosnippets = true,
+
+		-- Crazy highlights!!
+		-- #vid3
+		-- ext_opts = nil,
+		ext_opts = {
+			[types.choiceNode] = {
+				active = {
+					virt_text = { { " « ", "NonTest" } },
+				},
+			},
+		},
+	})
+
+	-- <c-up> is my expansion key
+	-- this will expand the current item or jump to the next item within the snippet.
+	vim.keymap.set({ "i", "s" }, "<c-up>", function()
+		if ls.expand_or_jumpable() then
+			ls.expand_or_jump()
+		end
+	end, { silent = true })
+
+	-- <c-j> is my jump backwards key.
+	-- this always moves to the previous item within the snippet
+	vim.keymap.set({ "i", "s" }, "<c-down>", function()
+		if ls.jumpable(-1) then
+			ls.jump(-1)
+		end
+	end, { silent = true })
+
+	-- <c-l> is selecting within a list of options.
+	-- This is useful for choice nodes (introduced in the forthcoming episode 2)
+	vim.keymap.set("i", "<c-right>", function()
+		if ls.choice_active() then
+			ls.change_choice(1)
+		end
+	end)
+end
+
+return M
