bca89e33b063cf4da4bf779582d53a538a1bd741
Vicente Espejo <viespejo@gmail.com>
2024-02-16 19:40:34 +0100

    enable tsserver diagnostics

--- lua/user/lspsettings/tsserver.lua
+++ lua/user/lspsettings/tsserver.lua
@@ -1,14 +1,14 @@
 return {
-  disable_diagnostics = true,
-  custom_on_attach = function(on_attach)
-    return function(client, bufnr)
-      on_attach(client,bufnr)
+	-- disable_diagnostics = true,
+	custom_on_attach = function(on_attach)
+		return function(client, bufnr)
+			on_attach(client, bufnr)
 
-      -- we use null-ls for diagnostic and formatting (esling and prettier)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
-      -- this only stops displaying the diagnostics but continues to receive them from the server
-      -- vim.diagnostic.disable(bufnr, vim.lsp.diagnostic.get_namespace(client.id))
-    end
-  end,
+			-- we use null-ls for diagnostic and formatting (esling and prettier)
+			client.server_capabilities.documentFormattingProvider = false
+			client.server_capabilities.documentRangeFormattingProvider = false
+			-- this only stops displaying the diagnostics but continues to receive them from the server
+			-- vim.diagnostic.disable(bufnr, vim.lsp.diagnostic.get_namespace(client.id))
+		end
+	end,
 }
