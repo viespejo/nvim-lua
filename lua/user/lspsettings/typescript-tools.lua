34f14cce640c8fab2f54a6033e7c9cfb4335d51f
Vicente Espejo <viespejo@gmail.com>
2024-04-24 17:44:50 +0200

    null-ls for formatting in typescript

--- lua/user/lspsettings/typescript-tools.lua
+++ lua/user/lspsettings/typescript-tools.lua
@@ -3,6 +3,10 @@ return {
 	custom_on_attach = function(on_attach)
 		return function(client, bufnr)
 			on_attach(client, bufnr)
+			-- we use null-ls for formatting (prettier)
+			client.server_capabilities.documentFormattingProvider = false
+			client.server_capabilities.documentRangeFormattingProvider = false
+
 			vim.api.nvim_buf_set_keymap(
 				bufnr,
 				"n",
@@ -10,6 +14,13 @@ return {
 				"<cmd>TSToolsGoToSourceDefinition<cr>",
 				{ noremap = true, silent = true }
 			)
+
+			-- check two-shlash plugin is installed
+			local ok, twoslash = pcall(require, "twoslash-queries")
+			if ok then
+				twoslash.attach(client, bufnr)
+				vim.keymap.set("n", "<leader>kk", "<cmd>TwoslashQueriesInspect<cr>", {})
+			end
 		end
 	end,
 }
