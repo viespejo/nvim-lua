c8371e2c191a31375024f5ce343a8495ed2dcc52
Vicente Espejo <viespejo@gmail.com>
2024-04-24 17:47:33 +0200

    two-slash plugin

--- lua/user/lspsettings/tsserver.lua
+++ lua/user/lspsettings/tsserver.lua
@@ -9,6 +9,13 @@ return {
 			client.server_capabilities.documentRangeFormattingProvider = false
 			-- this only stops displaying the diagnostics but continues to receive them from the server
 			-- vim.diagnostic.disable(bufnr, vim.lsp.diagnostic.get_namespace(client.id))
+
+			-- check two-shlash plugin is installed
+			local ok, twoslash = pcall(require, "twoslash-queries")
+			if ok then
+				twoslash.attach(client, bufnr)
+				vim.keymap.set("n", "<leader>k", "<cmd>TwoslashQueriesInspect<cr>", {})
+			end
 		end
 	end,
 }
