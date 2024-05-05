4fa61300c549c41dc18d3df64e11a54924f4b417
Vicente Espejo <viespejo@gmail.com>
2024-05-05 20:06:52 +0200

    chatgpt plugin: load with keys

--- lua/user/extras/chatgpt.lua
+++ lua/user/extras/chatgpt.lua
@@ -1,14 +1,31 @@
 local M = {
 	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("chatgpt").setup()
-	end,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"nvim-lua/plenary.nvim",
 		"folke/trouble.nvim",
 		"nvim-telescope/telescope.nvim",
+	},
+	keys = {
+		{ "<leader>Cc", "<cmd>ChatGPT<cr>", mode = "n", desc = "ChatGPT" },
+		{ "<leader>CC", "<cmd>ChatGPTCompleteCode<cr>", mode = "n", desc = "ChatGPT Complete Code" },
+		{ "<leader>Ca", "<cmd>ChatGPTActAs<cr>", mode = "n", desc = "ChatGPT Act As" },
+		{
+			"<leader>Ce",
+			"<cmd>ChatGPTEditWithInstruction<cr>",
+			mode = { "n", "v" },
+			desc = "ChatGPT Edit With Instruction",
+		},
+		{ "<leader>Ct", "<cmd>ChatGPTRun translate<cr>", mode = { "n", "v" }, desc = "ChatGPT Translate" },
+		{ "<leader>Cs", "<cmd>ChatGPTRun summarize<cr>", mode = { "n", "v" }, desc = "ChatGPT Summarize" },
+		{
+			"<leader>Cg",
+			"<cmd>ChatGPTRun grammar_correction<cr>",
+			mode = { "n", "v" },
+			desc = "ChatGPT Grammar Correction",
+		},
+		{ "<leader>Co", "<cmd>ChatGPTRun optimize_code<cr>", mode = { "n", "v" }, desc = "ChatGPT Optimize Code" },
+		{ "<leader>Cx", "<cmd>ChatGPTRun explain_code<cr>", mode = { "n", "v" }, desc = "ChatGPT Explain Code" },
 	},
 }
 
