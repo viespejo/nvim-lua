f96c1132f27301bc130e2f34e26145445b478e06
Vicente Espejo <viespejo@gmail.com>
2024-05-05 19:26:16 +0200

    move chatgpt prompt out of the lua folder

--- lua/user/extras/chatgpt.lua
+++ lua/user/extras/chatgpt.lua
@@ -16,9 +16,7 @@ function M.config()
 	local home = vim.fn.expand("$HOME")
 	require("chatgpt").setup({
 		-- api_key_cmd = "gpg --decrypt " .. home .. "/api-key-chatgpt.gpg",
-		predefined_chat_gpt_prompts = "file:///"
-			.. home
-			.. "/.test-nvim/config/nvim/lua/user/extras/chatgpt-prompts.csv",
+		predefined_chat_gpt_prompts = "file:///" .. vim.fn.stdpath("config") .. "/chatgpt-prompts.csv",
 	})
 end
 
